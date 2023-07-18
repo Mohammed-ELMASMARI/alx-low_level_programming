1d11414707c98cde234eb61ee91aa1c76748e036
Mohammed-ELMASMARI <elmasmari.mohamed@gmail.com>
2023-07-18 10:00:22 -0700

    0-putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-int _puchtar(char c);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -6,7 +6,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-woid jack_bauer(void);
+void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n); 
